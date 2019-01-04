e1d1fcfb68141cbb4c73c0a7d2a29b914bc4e401
DavidyukVova <vovkadavidyuk@gmail.com>
2019-01-04 17:53:50 +0200

    fourth commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    //bx slider
     $('.slider').bxSlider();
+});
+'use strict';
+
+var btns = Array.from(document.querySelectorAll('.btn_control'));
+var prgs = Array.from(document.querySelectorAll('.widget_block'));
+
+btns.map(function (btn) {
+    btn.addEventListener('click', function () {
+        // $('.widget_block').remove('active');
+        var _this = this;
+
+        prgs.map(function (prg) {
+            _this.dataset.id == prg.dataset.id ? prg.style.display = 'block' : prg.style.display = "none";
+        });
+    });
 });
